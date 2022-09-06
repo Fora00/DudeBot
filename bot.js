b2dfefc263e9e1a1300b1424144cfa84a29df1a9
Fora00 <francesco.elia.foradori@gmail.com>
2022-09-06 14:39:15 +0200

    light refactor

--- bot.js
+++ bot.js
@@ -4,11 +4,11 @@ import pullDude from './pullDudexpress.mjs';
 import cron from 'node-cron';
 import express from 'express';
 const app = express();
-
 dotenv.config();
+
 const TELEGRAM_KEY = process.env.TELEGRAM_KEY;
 const CHAT_ID = process.env.CHAT_ID;
-
+const PORT = process.env.PORT || 3000;
 const bot = new Telegraf(TELEGRAM_KEY);
 
 bot.start((context) => {
@@ -37,29 +37,21 @@ bot.hears('id', (ctx) => {
 cron.schedule('0 7 * * *', function () {
   pullDude()
     .then(function (result) {
+      const res = '';
       if (new Date(review.pubDate) >= new Date()) {
-        const res = `${result.title} è l'ultima recensione uscita (${result.link[0]}) ! [@Uroboro00](tg://user?id=174784018),  [@nostalgiaz](tg://user?id=11614517), [Angelo](tg://user?id=1602351576), [Chiara](tg://user?id=158594735), [@Pulvi88](tg://user?id=788802936) , [Bruno](tg://user?id=2128787533)`;
-
-        bot.telegram.sendMessage(CHAT_ID, res, {
-          parse_mode: 'markdown',
-          disable_web_page_preview: false,
-        });
+        res = `${result.title} è l'ultima recensione uscita (${result.link[0]}) ! [@Uroboro00](tg://user?id=174784018),  [@nostalgiaz](tg://user?id=11614517), [Angelo](tg://user?id=1602351576), [Chiara](tg://user?id=158594735), [@Pulvi88](tg://user?id=788802936) , [Bruno](tg://user?id=2128787533)`;
       } else {
-        bot.telegram.sendMessage(
-          CHAT_ID,
-          'nessuna nuova review  [Angelo](tg://user?id=1602351576), [Chiara](tg://user?id=158594735), i dudes sono nelle vostre mani per i social!  ',
-          {
-            parse_mode: 'markdown',
-            disable_web_page_preview: false,
-          }
-        );
+        res =
+          'nessuna nuova review  [Angelo](tg://user?id=1602351576), [Chiara](tg://user?id=158594735), i dudes sono nelle vostre mani per i social!  ';
       }
+      bot.telegram.sendMessage(CHAT_ID, res, {
+        parse_mode: 'markdown',
+        disable_web_page_preview: false,
+      });
     })
     .catch((err) => console.log(err));
 });
 bot.launch();
-
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function () {
   console.log(`Server is running at port ${PORT}`);
