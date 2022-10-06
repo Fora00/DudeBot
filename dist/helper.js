95276974137f74edd3b89590d30487787f186094
Francesco Elia Foradori <francescoeliaforadori@ForaMac.homenet.telecomitalia.it>
2022-10-06 23:57:09 +0200

    time to test date

--- dist/helper.js
+++ dist/helper.js
@@ -4,6 +4,7 @@ exports.testDate = void 0;
 const testDate = (dateToTest) => {
     const today = new Date().toISOString().substring(0, 10);
     const test = new Date(dateToTest).toISOString().substring(0, 10);
+    console.log(`TODAY -> ${today} - TEST -> ${test}`);
     return test === today;
 };
 exports.testDate = testDate;
--- helper.ts
+++ helper.ts
@@ -1,5 +1,7 @@
 export const testDate = (dateToTest: string | number | Date): boolean => {
   const today = new Date().toISOString().substring(0, 10);
   const test = new Date(dateToTest).toISOString().substring(0, 10);
+  console.log(`TODAY -> ${today} - TEST -> ${test}`);
+  
   return test === today;
 };
